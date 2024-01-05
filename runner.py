facf78bb6c8da4d4aca417b38173d3534fd747e1
Some Coder <87237568+SomeSuperCoder@users.noreply.github.com>
2024-01-05 12:52:02 +0300

    Class split

---
+++ runner.py
@@ -0,0 +1,33 @@
+#######################################
+# IMPORTS
+#######################################
+
+
+from Lexer import *
+from Parser import *
+from GlobalSymbolTable import global_symbol_table
+from Interpreter import *
+
+#######################################
+# RUN
+#######################################
+
+
+def run(fn, text):
+    # Generate tokens
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    if error: return None, error
+
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    if ast.error: return None, ast.error
+
+    # Run program
+    interpreter = Interpreter()
+    context = Context('<program>')
+    context.symbol_table = global_symbol_table
+    result = interpreter.visit(ast.node, context)
+
+    return result.value, result.error
--- shell.py
+++ shell.py
@@ -1,9 +1,9 @@
-import basic
+import runner
 
 while True:
     text = input('basic > ')
     if text.strip() == "": continue
-    result, error = basic.run('<stdin>', text)
+    result, error = runner.run('<stdin>', text)
 
     if error:
         print(error.as_string())
